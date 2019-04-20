a0602a33fb22cdda2c8869a23b3ce40259065c56
Andrey Kropotin <tork713@gmail.com>
2019-04-20 07:36:20 +0700

    20.04.2019_7:36

--- Inherit/Inherit.py
+++ Inherit/Inherit.py
@@ -1,1 +1,27 @@
+class SchoolMember(object):
+    '''Представляет любого человека в школе'''
+
+    # Инициализация класса
+    def __init__(self,name,age): # У экземпляра будет имя и возраст
+        self.name = name
+        self.age = age
+        print('Создан SchoolMember: {0}'.format(self.name))
+    #************************************************
+
+    def tell(self):
+        '''Вывести информацию'''
+        print('Имя: "{0}", Возраст: "{1}"'.format(self.name,self.age,end = " "))
+    #************************************************
+
+#   КЛАССЫ НАСЛЕДНИКИ
+
+class Teacher(SchoolMember):      # класс Teacher наследник класса SchoolMember  
+    '''Представляет преподавателя'''
+    def __init__(self, name, age, salary):
+        SchoolMember.__init__(self, name, age)
+        self.salary = salary
+        print('Создан Teacher: {0}'.format(self.name))
+
+
+
 
